6ac7a9878b4bf681909f888a668b24a4ffeffca9
Andreas Kanz <Andreas.Kanz@anacision.de>
2021-10-02 17:29:41 +0200

    feat: :card_file_box: add basic models

--- recipe_project/main/admin.py
+++ recipe_project/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredient, Recipe, RecipeIngredient
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient)
--- recipe_project/main/models.py
+++ recipe_project/main/models.py
@@ -1,3 +1,38 @@
+from autoslug import AutoSlugField
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to="images/", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=150)
+    slug = AutoSlugField(populate_from="title", unique=True)
+    ingredients = models.ManyToManyField("Ingredient", through="RecipeIngredient")
+    description = models.TextField()
+    instructions = models.TextField()
+    time_required = models.PositiveBigIntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to="images/", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey("Recipe", on_delete=models.CASCADE)
+    ingredient = models.ForeignKey("Ingredient", on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    unit = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.recipe.title} - {self.ingredient.title}"
